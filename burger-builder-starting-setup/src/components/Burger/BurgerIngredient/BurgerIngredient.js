f3cc03679f7f84c542aa4a000976cced119e713f
Tyler Frick <tyfrick25@gmail.com>
2020-08-05 16:31:20 -0700

    added react hooks

--- burger-builder-starting-setup/src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ burger-builder-starting-setup/src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import classes from './BurgerIngredient.css';
 
-class BurgerIngredient extends Component {
+const burgerIngredient = props => {
 
-    render () {
         let ingredient = null;
-        switch ( this.props.type) {
+        switch ( props.type) {
             case ('bread-bottom'):
                 ingredient = <div className={classes.BreadBottom}></div>;
                 break;
@@ -35,11 +34,10 @@ class BurgerIngredient extends Component {
                 ingredient = null;
         }
         return ingredient;
-    }
 };
 
-BurgerIngredient.propTypes = {
+burgerIngredient.propTypes = {
     type: PropTypes.string.isRequired
 };
 
-export default BurgerIngredient;
+export default burgerIngredient;
--- burger-builder-starting-setup/src/hoc/Layout/Layout.js
+++ burger-builder-starting-setup/src/hoc/Layout/Layout.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 
 import Aux from '../Aux/Aux';
@@ -6,37 +6,31 @@ import classes from './Layout.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
-class Layout extends Component {
-    state = {
-        showSideDrawer : false
+const layout = props => {
+    const [sideDrawerIsVisibe, setSideDrawerVisibility] = useState(false);
+
+    const sideDrawerClosedHandler = () => {
+        setSideDrawerVisibility(false);
     }
 
-    sideDrawerClosedHandler = () => {
-        this.setState({showSideDrawer: false});
+    const sideDrawerOpenHandler = () => {
+        setSideDrawerVisibility(!sideDrawerIsVisibe);
     }
 
-    sideDrawerOpenHandler = () => {
-        this.setState((prevState) => {
-            return { showSideDrawer: !prevState.showSideDrawer};
-        });
-    }
-
-    render () {
         return (
             <Aux>
                 <Toolbar 
-                    isAuth={this.props.isAuthenticated}
-                    drawerToggleClicked={this.sideDrawerOpenHandler}/>
+                    isAuth={props.isAuthenticated}
+                    drawerToggleClicked={sideDrawerOpenHandler}/>
                 <SideDrawer 
-                    isAuth={this.props.isAuthenticated}
-                    open={this.state.showSideDrawer} 
-                    closed={this.sideDrawerClosedHandler}/>
+                    isAuth={props.isAuthenticated}
+                    open={sideDrawerIsVisibe} 
+                    closed={sideDrawerClosedHandler}/>
                 <main className={classes.Content}>
-                    {this.props.children}
+                    {props.children}
                 </main>
             </Aux>
         )
-    }
 };
 
 const mapStateToProps = state => {
@@ -45,4 +39,4 @@ const mapStateToProps = state => {
     }
 }
 
-export default connect(mapStateToProps)(Layout);
+export default connect(mapStateToProps)(layout);
